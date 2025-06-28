032e46bd4923c53250ef820066195c54d76e6035
Harshit-077 <harshitsharma.0777@gmail.com>
2025-06-28 16:34:17 +0530

    Made an AI Bot using ChatGPT wrapper with basic input and output

---
+++ Agent_Bot.py
@@ -0,0 +1,29 @@
+from typing import List, TypedDict
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    message: List[HumanMessage]
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state["message"])
+    print(f"\nAI: {response.content}\n")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process",process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+user_input = input("Enter: ")
+while user_input != "exit":
+    agent.invoke({"message": [HumanMessage(content=user_input)]})
+    user_input = input("Enter: ")
+
